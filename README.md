b9943c3527113f2fa1026e7cd4f543cf1ccad9f1
Chris Marsh <chrismarsh.c2@gmail.com>
2021-01-29 15:25:02 -0600

    migrate to GHAP

--- README.md
+++ README.md
@@ -1,21 +1,11 @@
-Conan build script for the GNU Multiple Precision Arithmetic library (GMP)
+# mpfr
 
-Build
---
+This is a minimal Conan build of the mpfr library for use with [CHM](https://github.com/Chrismarsh/CHM). 
 
-`conan create mpfr/4.0.1@ntc/stable -pr <profile>`
+Build artifacts are uploaded to [Bintray](https://bintray.com/chrismarsh/CHM)
 
-Notes
---
 
-- This was created in order to build CGAL
-- This should build directly with g++
-- For MSVC, this is cross-compiled using the `msys2_mingw` profile (see
-  `profiles/msys2_mingw`, or better yet, the [Windows
-  Subsystem](http://docs.conan.io/en/latest/systems_cross_building/windows_subsystems.html?highlight=msys2_mingw)
-  Conan docs.)  The `package_id()` function then makes the package appear as if
-  it was build with Microsoft Visual Studio.
+```
+conan install mpfr/4.1.0@CHM/stable
+```
 
-See also:
-- [conan-gmp](https://github.com/kheaactua/conan-gmp).
-- [conan-build-helpers](https://github.com/kheaactua/conan-build-helpers).
--- conanfile.py
+++ conanfile.py
@@ -5,9 +5,8 @@ class MpfrConan(ConanFile):
     """ Building MPFR for the intention of using it to build CGAL """
 
     name        = 'mpfr'
-    version     = '4.1.0'
     description = 'The GNU Multiple Precision Arithmetic Library'
-    url         = 'https://github.com/CGAL/conan-mpfr'
+    url         = 'https://github.com/Chrismarsh/conan-mpfr'
     license     = 'MIT'
     settings    = 'os', 'compiler', 'arch', 'build_type'
     requires    = 'gmp/[>=5.0.0]@CHM/stable'
@@ -15,27 +14,15 @@ class MpfrConan(ConanFile):
     # See http://www.mpfr.org/mpfr-current/mpfr.pdf for other potential options
     options = {
         'shared':            [True, False],
-        'static':            [True, False],
-        'msvc':              [12, 15],
+        'static':            [True, False]
     }
-    default_options = 'shared=True', 'static=True', 'msvc=12'
+    default_options = 'shared=True', 'static=True'
 
     def source(self):
-
-        archive = 'mpfr-{version}.tar.gz'.format(version=self.version)
-        tools.download('http://www.mpfr.org/mpfr-current/{archive}'.format(archive=archive), archive)
-        tools.unzip(archive)
+        tools.get(**self.conan_data["sources"][self.version])
+       
         shutil.move('mpfr-{version}'.format(version=self.version), self.name)
         tools.replace_in_file(self.name+"/configure", r"-install_name \$rpath/", "-install_name @rpath/")
-        os.unlink(archive)
-
-    def configure(self):
-        if 'Windows' == self.settings.os:
-            # On Windows, we can only build the static OR shared
-            self.options.static = not self.options.shared
-
-            # Shared MPFR requires a GMP DLL
-            self.options['gmp'].shared = self.options.shared
 
     def build(self):
         with tools.chdir(self.name):
@@ -44,19 +31,16 @@ class MpfrConan(ConanFile):
             env_vars = {}
             args = []
 
-            if 'gcc' == self.settings.compiler and 'Windows' == platform.system():
-                args.append('--prefix=%s'%tools.unix_path(self.package_folder))
-            else:
-                args.append('--prefix=%s'%self.package_folder)
+            args.append('--prefix=%s'%self.package_folder)
 
             args.append('--%s-shared'%('enable' if self.options.shared else 'disable'))
             args.append('--%s-static'%('enable' if self.options.static else 'disable'))
 
-            if self.settings.os == "Linux" or self.settings.os == "Macos":
-                autotools.fpic = True
-                if self.settings.arch == 'x86':
-                    env_vars['ABI'] = '32'
-                    autotools.cxx_flags.append('-m32')
+
+            autotools.fpic = True
+            if self.settings.arch == 'x86':
+                env_vars['ABI'] = '32'
+                autotools.cxx_flags.append('-m32')
 
             # Add GMP
             autotools.library_paths.append(os.path.join(self.deps_cpp_info['gmp'].rootpath, self.deps_cpp_info['gmp'].libdirs[0]))
@@ -70,9 +54,6 @@ class MpfrConan(ConanFile):
                 autotools.configure(args=args)
 
             autotools.make()
-            if 'gcc' == self.settings.compiler and 'Windows' == self.settings.os:
-                if self.env['DLLTOOL'] is not None:
-                    self.run('{dlltool} --output-lib mpfr.lib --input-def src/.libs/libmpfr-6.dll.def --dllname libmpfr-6.dll'.format(dlltool=self.env['DLLTOOL']))
             autotools.make(args=['install'])
 
     def package(self):
@@ -81,17 +62,4 @@ class MpfrConan(ConanFile):
 
     def package_info(self):
         self.cpp_info.libs = ['mpfr']
-
-    def package_id(self):
-        # On windows, we cross compile this with mingw.. But because it's
-        # compatible with MSVC, set it's hash to reflect that.
-        # Maybe use tools.cross_building(self.settings)
-        if 'gcc' == self.settings.compiler and 'Windows' == self.settings.os:
-            self.info.settings.compiler = 'Visual Studio'
-            self.info.settings.compiler.version = int(str(self.options.msvc))
-
-            runtime = 'MD' if self.options.shared else 'MT'
-            if self.settings.build_type == 'Debug':
-                runtime += 'd'
-            self.info.settings.compiler.runtime = runtime
 
